931ed7f21cb1a440cfe20555cba82d3d93ad7253
wpjscc <76907477+wpjscc@users.noreply.github.com>
2023-09-20 23:30:30 +0800

    Update Connection.php

--- tests/helpers/Ratchet/Mock/Connection.php
+++ tests/helpers/Ratchet/Mock/Connection.php
@@ -17,4 +17,8 @@ class Connection implements ConnectionInterface {
     public function close() {
         $this->last[__FUNCTION__] = true;
     }
+
+    public function getConnection() {
+        return $this->last;
+    }
 }
